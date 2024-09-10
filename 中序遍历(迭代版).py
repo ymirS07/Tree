054510812fcf8bde5f225a910adcb86ff5ced0eb
Siqi Yang <144865198+ymirS07@users.noreply.github.com>
2024-09-10 22:32:15 +0800

    Create 中序遍历(迭代版).py

---
+++ 中序遍历(迭代版).py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        st = []
+        res = []
+        p = root
+        while p or st:
+            if p: #直接走到最左开始
+                st.append(p)
+                p = p.left
+            else: #然后按中-->右的顺序加入res 这个condition就类似谦虚前序的右
+                p = st.pop()
+                res.append(p.val)
+                p = p.right
+        return res
